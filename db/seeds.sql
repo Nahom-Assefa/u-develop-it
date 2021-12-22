ef72ec5c2002e8a6a0ecc9d9bf123857b57d9c5f
Nahom Assefa <nzenebe@gmail.com>
2021-12-22 14:44:01 -0600

    push 12.1.6

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES 
+    ('Bitcoin', 'BTC', 1),
+    ('Avalanche', 'AJAX', 1),
+    ('Cardano', 'ADA', 0),
+    ('Ethereum', 'ETH', 1),
+    ('Polkadot', 'DOT', 1),
+    ('Vechain', 'VET', 0),
+    ('Algorand', 'ALGO', 0),
+    ('Quant', 'QNT', 1),
+    ('Luna', 'Terra', 1),
+    ('Tezos', 'XTZ', 1);
+
+-- INSERT INTO candidates (first_name, last_name, industry_connected)
+-- VALUES
+--   ('Ronald', 'Firbank', 1),
+--   ('Virginia', 'Woolf', 1),
+--   ('Piers', 'Gaveston', 0),
+--   ('Charles', 'LeRoi', 1),
+--   ('Katherine', 'Mansfield', 1),
+--   ('Dora', 'Carrington', 0),
+--   ('Edward', 'Bellamy', 0),
+--   ('Montague', 'Summers', 1),
+--   ('Octavia', 'Butler', 1),
+--   ('Unica', 'Zurn', 1);
